1c20e8e1d782c401875998509834e8d72a8bebe6
Viktror <supermario.21@mail.ru>
2019-10-01 17:34:46 +0300

    fix hello.py N3

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def application(env, start_response):
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    body = [bytes(i + '\n', 'ascii') for i in env['QUERY_STRING'].split('&')]
     start_response('200 OK', [('Content-Type', 'text/plain')])
     return body
