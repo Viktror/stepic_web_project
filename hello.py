a3dd3829adbdc04b74942a500bbd5887a873f9ac
Viktror <supermario.21@mail.ru>
2019-10-02 10:16:28 +0300

    fix hello N1

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
+    print([bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")])
     return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
                   encoding="utf8")]
